5d6229e1f83282acacdac7700e4c442e50621509
mahaoyang <mahydxygitlab>
2019-03-21 12:07:31 +0800

    init

--- mysql/mysql.py
+++ mysql/mysql.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 import pymysql
+import traceback
 
 
 class Mysql(object):
-    def __init__(self, batch_num=10000, *args, **kwargs):
+    def __init__(self, batch_size=10000, *args, **kwargs):
         kw = {'host': 'localhost',
               'user': 'root',
               'password': '1234',
@@ -17,46 +18,68 @@ class Mysql(object):
             self.con = pymysql.connect(*args, **kwargs)
         elif kwargs:
             self.con = pymysql.connect(*args, **kwargs)
-        self.batch_num = batch_num
+        self.batch_size = batch_size
 
-    def __execute_sql(self, sql):
+    @staticmethod
+    def __is_select(sql):
+        return True if 'SELECT' in sql.upper() else False
+
+    def execute_sql(self, sql):
         try:
             result = None
+            select_flag = False
             with self.con.cursor() as cursor:
                 if isinstance(sql, list):
-                    # for n in range(int(len(sql) / self.batch_num) + 1):
-                        for i in sql:
-                            result = cursor.execute(i)
+                    for i in sql:
+                        select_flag = self.__is_select(i)
+                        result = cursor.execute(i)
                 else:
+                    select_flag = self.__is_select(sql)
                     result = cursor.execute(sql)
             self.con.commit()
+            if select_flag:
+                return cursor.fetchall()
             return result
-        except:
+        except Exception as E:
             self.con.close()
+            return traceback.format_exc()
+
+    @staticmethod
+    def __slice(s, step):
+        res = []
+        quotient = len(s) // step
+        if quotient:
+            for index in range(quotient):
+                res.append(s[index:index + step])
+        else:
+            res.append(s)
+        return res
 
     def __columns_parser(self, table, sql, *args, **kwargs):
         res = []
         if args:
-            sql = "select COLUMN_NAME from information_schema.COLUMNS where table_name = '%s'" % table
-            columns = self.__execute_sql(sql)
-            if isinstance(args[0], dict):
-                for i in args:
-                    res.append(self.__one_columns_parser(sql, item=i))
-            elif isinstance(args[0], list):
-                for i in args:
-                    _columns = columns[:len(i)]
+            columns_sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE table_name = '%s'" % table
+            columns = [i['COLUMN_NAME'] for i in self.execute_sql(columns_sql)]
+            batch = self.__slice(args, self.batch_size)
+            for args in batch:
+                if isinstance(args[0], dict):
+                    for arg in args:
+                        res.append(self.__one_columns_parser(sql, item=arg))
+                elif isinstance(args[0], list):
+                    for arg in args:
+                        _columns = columns[:len(arg)]
+                        res.append(self.__one_columns_parser(sql, item=dict(
+                            zip(_columns, arg))))
+                elif not isinstance(args[0], list):
+                    _columns = columns[:len(args)]
                     res.append(self.__one_columns_parser(sql, item=dict(
-                        zip(_columns, i))))
-            elif not isinstance(args[0], list):
-                _columns = columns[:len(args)]
-                res.append(self.__one_columns_parser(sql, item=dict(
-                    zip(_columns, args))))
+                        zip(_columns, args))))
         if kwargs:
             res.append(self.__one_columns_parser(sql, item=kwargs))
         return res
 
     @staticmethod
-    def __one_columns_parser(sql: str, item: dict):
+    def __one_columns_parser(sql, item):
         columns = ''
         values = ''
         for key, value in item.items():
@@ -70,24 +93,31 @@ class Mysql(object):
         sql = sql.format(columns=columns, values=values)
         return sql
 
-    def insert(self, table, ignore=True, *args, **kwargs):
-        sql = "INSERT INTO `%s` ({columns}) VALUES (values)" % table
-        if ignore:
-            sql = "INSERT IGNORE INTO `%s` (`name`) VALUES (values)" % table
-        sql = self.__columns_parser(table=table, sql=sql, *args, **kwargs)
-        self.execute(sql)
+    def insert(self, table, *args, **kwargs):
+        sql = "INSERT INTO `%s` ({columns}) VALUES ({values})" % table
+        sqls = self.__columns_parser(table, sql, *args, **kwargs)
+        for sql in sqls:
+            self.execute_sql(sql)
+
+    def insert_ignore(self, table, *args, **kwargs):
+        sql = "INSERT IGNORE INTO `%s` ({columns}) VALUES ({values})" % table
+        sqls = self.__columns_parser(table, sql, *args, **kwargs)
+        for sql in sqls:
+            self.execute_sql(sql)
 
     def replace(self, table, *args, **kwargs):
-        sql = "REPLACE INTO `%s` ({columns}) VALUES (values)" % table
-        sql = self.__columns_parser(table=table, sql=sql, *args, **kwargs)
-        self.execute(sql)
+        sql = "REPLACE INTO `%s` ({columns}) VALUES ({values})" % table
+        sqls = self.__columns_parser(table, sql, *args, **kwargs)
+        for sql in sqls:
+            self.execute_sql(sql)
 
     def delete(self, table, *args, **kwargs):
-        sql = "DELETE FROM `%s`  WHERE {columns} = values" % table
-        sql = self.__columns_parser(table=table, sql=sql, *args, **kwargs)
-        self.execute(sql)
+        sql = "DELETE FROM `%s`  WHERE {columns} = {values}" % table
+        sqls = self.__columns_parser(table, sql, *args, **kwargs)
+        for sql in sqls:
+            self.execute_sql(sql)
 
-    def __sql_parser(self, text: str, *args, **kwargs):
+    def __sql_parser(self, text, *args, **kwargs):
         res = []
         if args:
             if isinstance(args[0], dict) and '{' in text:
@@ -105,13 +135,27 @@ class Mysql(object):
 
     def execute(self, text, *args, **kwargs):
         sql = self.__sql_parser(text, *args, **kwargs)
-        self.__execute_sql(sql)
+        self.execute_sql(sql)
 
 
 if __name__ == '__main__':
     mysql = Mysql(password='rootroot', db='spider_system')
     sql = "INSERT INTO `test` (`name`) VALUES (\'{name}\')"
-    val = [{'name': 'test'}, {'name': 'test'}]
-    # val = [['test'], ['test']]
-    for i in range(100):
-        mysql.execute(sql, *val)
+    # val = [{'name': 'test'}, {'name': 'test'}]
+    # for i in range(100):
+    #     mysql.execute(sql, *val)
+    # val = [1]
+    # val = [1, 'test']
+    # val = [[1, 'test'], [2, 'test']]
+    # val = {'id': 1, 'name': 'test1'}
+    val = [{'id': 1, 'name': 'test'}, {'id': 2, 'name': 'test'}]
+    # mysql.replace('test', *val)
+    mysql.execute(sql, *val)
+    # mysql.insert('test', *val)
+    # mysql.insert_ignore('test', *val)
+    # val = [1]
+    # val = [[1], [2]]
+    # val = {'id': 1}
+    # val = [{'id': 1}, {'id': 2}]
+    # mysql.delete('test', *val)
+    # mysql.execute_sql("TRUNCATE TABLE test;")
