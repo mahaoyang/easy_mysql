e8d472a4764e7636b060c102d086a1e58e9d7ab8
mahaoyang <mahydxygitlab>
2019-03-21 16:59:24 +0800

    init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='masql',
-      version='0.0.1',
+      version='0.0.2',
       description='mysql for humans',
       long_description=open('README.md').read(),
       author='Ma HaoYang',
